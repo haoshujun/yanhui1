0b0e6fb1820aae0f1461fd56660b1713d2a18963
luyao <1624652155@qq.com>
2021-07-17 14:59:57 +0800

    1111

--- ngbusi/src/test.java
+++ ngbusi/src/test.java
@@ -2,7 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("我好");
-        System.out.println("大家好");
-        System.out.println("haha");
+        System.out.println("hehe");
     }
 }
