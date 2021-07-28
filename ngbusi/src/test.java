e0bc50ab1a0ee92b087e321dc76480536a4938c1
luyao <1624652155@qq.com>
2021-07-28 11:04:32 +0800

    c1

--- ngbusi/src/test.java
+++ ngbusi/src/test.java
@@ -1,12 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("cc");
 
     }
-    public void ccc(){
-        System.out.println("22");
-    }
-    public void add(){
-        System.out.println(11);
-    }
+
 }
